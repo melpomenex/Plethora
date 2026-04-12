760849497da8c58a3a6ebcc99764b6f20ca6d5bd
melpomenex <10539567+melpomenex@users.noreply.github.com>
2026-04-12 08:02:55 +0000

    release 1.19.1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@
  */
 
 export const APP_NAME = "Incrementum";
-export const APP_VERSION = "1.19.0";
+export const APP_VERSION = "1.19.1";
 
 export const KEYBOARD_SHORTCUTS = {
   COMMAND_PALETTE: "Cmd+Shift+P",
